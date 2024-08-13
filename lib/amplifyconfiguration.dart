85dbd6ff545441607d16617fe93a275b6825cb0a
Esperanza31 <166470594+Esperanza31@users.noreply.github.com>
2024-08-13 10:40:24 +0800

    Add files via upload

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,17 @@
+const amplifyconfig = '''{
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "MOOVITANEPS": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://u3kc2auazbc2lgxa6lqv554haq.appsync-api.ap-southeast-2.amazonaws.com/graphql",
+                    "region": "ap-southeast-2",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-z2qkad3tnjfjnipxbf7oszjkmm"
+                }
+            }
+        }
+    }
+}''';
